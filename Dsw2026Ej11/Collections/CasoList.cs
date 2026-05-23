eaee2d56ed2d90065c617c788e86c7028d94c9ce
Belen <MariaBelenLeyva@alu.frt.utn.edu.ar>
2026-05-22 21:40:46 -0300

    Completado el punto 5 con correccion de formatos regionales

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,39 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    // Campo privado para la lista de alumnos
+    private readonly List<Alumno> _alumnos = new List<Alumno>();
+
+    // Incluir un método para agregar alumnos a la lista
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _alumnos.Add(alumno);
+    }
+
+    // Incluir un método para retornar la lista
+    public List<Alumno> ObtenerAlumnos()
+    {
+        return _alumnos;
+    }
+
+    // Incluir un método para buscar un alumno por nombre
+    public Alumno? BuscarPorNombre(string nombre)
+    {
+        return _alumnos.Find(a => a.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    // Incluir un método para eliminar un alumno (debe recibir un alumno)
+    public bool EliminarAlumno(Alumno alumno)
+    {
+        return _alumnos.Remove(alumno);
+    }
+
+    // Incluir un método para eliminar un alumno en una determinada posición de la lista
+    public void EliminarEnPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < _alumnos.Count)
+        {
+            _alumnos.RemoveAt(posicion);
+        }
+    }
 }
